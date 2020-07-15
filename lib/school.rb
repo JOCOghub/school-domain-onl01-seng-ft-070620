e80c74b1b902e31b33b938fab030cfe3729a12b6
Joseph cohen <jojocohen57@gmail.com>
2020-07-15 17:24:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,7 @@ class School
   end
   
   def add_student(new_name, grade)
-     @roster[grade] ||= []
+     @roster[grade] ||= [] #if we already used a grade key we would accidently erase its contents before adding another name so have to use ||= to set condition that if the specific grade key already exists it wont set it back equal to an empty array 
      @roster[grade] << new_name
   end 
   
